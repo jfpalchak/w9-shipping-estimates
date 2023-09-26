9b7d046e3fac6943933c95a7d4e52ccb16d49c1b
Joey Palchak <joey.palchak@gmail.com>
2023-09-26 13:33:44 -0700

    add Controller + form & display for Index View

--- ShippingEstimates/Controllers/HomeController.cs
+++ ShippingEstimates/Controllers/HomeController.cs
@@ -5,6 +5,18 @@ namespace ShippingEstimates.Controllers
 {
   public class HomeController : Controller
   {
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
 
+    [HttpPost("/parcels")]
+    public ActionResult Create(float length, float width, float height, float weight)
+    {
+      Parcel package = new Parcel(length, width, height, weight);
+
+      return View("Index", package);
+    }
   }
 }
