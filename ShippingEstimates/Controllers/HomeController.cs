4958beabfa386b30e6bc3bf957276703225caddb
Joey Palchak <joey.palchak@gmail.com>
2023-09-26 15:29:14 -0700

    refactor Create route to ParcelsController

--- ShippingEstimates/Controllers/HomeController.cs
+++ ShippingEstimates/Controllers/HomeController.cs
@@ -10,13 +10,5 @@ namespace ShippingEstimates.Controllers
     {
       return View();
     }
-
-    [HttpPost("/parcels")]
-    public ActionResult Create(float length, float width, float height, float weight)
-    {
-      Parcel package = new Parcel(length, width, height, weight);
-
-      return View("Index", package);
-    }
   }
 }
--- ShippingEstimates/Controllers/ParcelsController.cs
+++ ShippingEstimates/Controllers/ParcelsController.cs
@@ -5,6 +5,12 @@ namespace ShippingEstimates.Controllers
 {
   public class ParcelsController : Controller
   {
-    
+    [HttpPost("/parcels")]
+    public ActionResult Create(float length, float width, float height, float weight)
+    {
+      Parcel package = new Parcel(length, width, height, weight);
+
+      return View("../Home/Index", package);
+    }
   }
 }
